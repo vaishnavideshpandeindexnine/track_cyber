a3f14cdf6c1e8c868a9435b85fae26ed09707fbf
Vaishnavi Deshpande <vaishnavi.deshpande@indexnine.com>
2024-09-30 17:45:48 +0530

    asd

--- script.js
+++ script.js
@@ -140,6 +140,7 @@ function updateUI(
     macDownloadButtons.style.display = "block";
     defaultDownloadButton.style.display = "none";
     platformText = "mac OS";
+    packageText = data.macOS.AppleSilicon.package;
 
     intelButton?.addEventListener("click", function () {
       console.log("clicked Intel");
